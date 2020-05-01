6ef32ac5165e87a255e1db2af8366a575cd0f387
Vadim Ermakov <vadim22.barnaul@gmail.com>
2020-05-01 22:24:46 +0300

    Main.cs

--- Main.cs
+++ Main.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Collections.Generic;
+
+namespace Monoalphabet
+{
+	public class StartForm : Form
+	{
+		private System.ComponentModel.Container components = null;
+
+		public StartForm()
+		{
+			InitializeComponent();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				if (components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose();
+		}
+
+		#region Windows Form Designer generated code
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.Size = new System.Drawing.Size(1024, 576);
+			this.Text = "Monoalphabet Cipher Implementation";
+		}
+		#endregion
+
+		[STAThread]
+		public static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new StartForm());
+
+			return;
+		}
+
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("Monoalphabet.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Monoalphabet.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("11f00c86-d140-44fa-87ec-4c6659f61ae4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
